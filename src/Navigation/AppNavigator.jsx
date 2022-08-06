827e7796db6c4f0a0c460e51c024e8621a1b866b
atharvapatil22 <atharva.22.patil@gmail.com>
2022-08-06 10:41:58 +0530

    Added styling to tab navigator

--- src/Navigation/AppNavigator.jsx
+++ src/Navigation/AppNavigator.jsx
@@ -4,27 +4,69 @@ import Stadiums from "../Screens/Stadiums";
 import Players from "../Screens/Players";
 import Tab3 from "../Screens/Tab3";
 import { MaterialCommunityIcons, Ionicons } from "react-native-vector-icons";
+import { Image, View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerRight: () => (
+          <Image
+            style={{ height: "100%", width: 50, marginRight: 20 }}
+            source={require("../assets/images/app_logo.png")}
+          />
+        ),
+        headerStyle: { fontFamily: "Uchen-Regular" },
+        tabBarLabelStyle: {
+          fontFamily: "sans-serif",
+          fontWeight: "bold",
+          fontSize: 12,
+        },
+        tabBarActiveTintColor: "green",
+        inactiveTintColor: "#9E9E9E",
+      }}
+    >
       <Tab.Screen
         name="Players"
         component={Players}
         options={{
-          tabBarIcon: () => <Ionicons name="people" size={25} />,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="people"
+              size={25}
+              color={focused ? "green" : "grey"}
+            />
+          ),
         }}
       />
       <Tab.Screen
         name="Stadiums"
         component={Stadiums}
         options={{
-          tabBarIcon: () => <MaterialCommunityIcons name="stadium" size={25} />,
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="stadium"
+              size={25}
+              color={focused ? "green" : "grey"}
+            />
+          ),
         }}
       />
-      <Tab.Screen name="Tab3" component={Tab3} />
+      <Tab.Screen
+        name="Tab3"
+        component={Tab3}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="home-floor-3"
+              size={25}
+              color={focused ? "green" : "grey"}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
